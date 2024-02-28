e1c449263b85fa208fe238975142f3a77b8ed4ae
Hai Zhu <35182391+cocolato@users.noreply.github.com>
2024-02-28 03:11:34 +0000

    use builtin function do print

--- pydumpling/helpers.py
+++ pydumpling/helpers.py
@@ -1,26 +1,12 @@
-import linecache
+from traceback import print_tb, print_exception
 
 
 def print_traceback_and_except(dumpling_result):
-    traceback_obj = dumpling_result["traceback"]
-    except_extra = dumpling_result["exc_extra"]
-    except_type = except_extra["exc_type"]
-    except_value = except_extra["exc_value"]
-
-    print("Traceback (most recent call last):")
-    while traceback_obj:
-        frame = traceback_obj.tb_frame
-        lineno = traceback_obj.tb_lineno
-        filename = frame.f_code.co_filename
-        function_name = frame.f_code.co_name
-        line = linecache.getline(filename, lineno).strip()
-        mid_index = len(line) // 2
-
-        print(f"File \"{filename}\", line {lineno}, in {function_name}")
-        print(f"  {line}")
-        print(" " * 4 + "~" * mid_index + "^" + "~" * mid_index)
-        traceback_obj = traceback_obj.tb_next
-
-        if traceback_obj is None:
-            break
-    print(f"{except_type.__name__}: {except_value}")
+    exc_tb = dumpling_result["traceback"]
+    except_extra = dumpling_result.get("exc_extra")
+    exc_type = except_extra["exc_type"] if except_extra else None
+    exc_value = except_extra["exc_value"] if except_extra else None
+    if exc_type and exc_value:
+        print_exception(exc_type, exc_value, exc_tb)
+    else:
+        print_tb(exc_tb)
