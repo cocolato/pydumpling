c346405ce0a585befff5b0b5128b0d8c93b5de51
Hai Zhu <35182391+cocolato@users.noreply.github.com>
2024-03-06 15:45:56 +0000

    support rpdb

--- pydumpling/debug_dumpling.py
+++ pydumpling/debug_dumpling.py
@@ -17,14 +17,16 @@ def load_dumpling(filename):
         except Exception:
             return pickle.load(f)
 
-
-def debug_dumpling(dump_file, pdb=pdb):
+def mock_inspect():
     inspect.isframe = lambda obj: isinstance(
         obj, types.FrameType) or obj.__class__ == FakeFrame
     inspect.istraceback = lambda obj: isinstance(
         obj, types.TracebackType) or obj.__class__ == FakeTraceback
     inspect.iscode = lambda obj: isinstance(
         obj, types.CodeType) or obj.__class__ == FakeCode
+
+def debug_dumpling(dump_file, pdb=pdb):
+    mock_inspect()
     dumpling = load_dumpling(dump_file)
     if not StrictVersion("0.0.1") <= StrictVersion(dumpling["version"]) < StrictVersion("1.0.0"):
         raise ValueError("Unsupported dumpling version: %s" %
