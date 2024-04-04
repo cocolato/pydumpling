7a0a3e2a049e930ca660f8aa126f36a7bd0b0172
cocolato <haiizhu@outlook.com>
2024-04-04 17:53:59 +0800

    support catch_any_exception

--- pydumpling/__init__.py
+++ pydumpling/__init__.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from .rpdb import r_post_mortem
+from .helpers import catch_any_exception
 from .debug_dumpling import debug_dumpling, load_dumpling
 from .pydumpling import save_dumping, dump_current_traceback, __version__
 
 
 __version__ == __version__
-__all__ = ["debug_dumpling", "load_dumpling", "save_dumping", "dump_current_traceback", "r_post_mortem"]
+__all__ = ["debug_dumpling", "load_dumpling", "save_dumping", "dump_current_traceback", "r_post_mortem", "catch_any_exception"]
